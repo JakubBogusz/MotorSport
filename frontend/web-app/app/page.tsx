e7180753fd864865ffcfbea6cf53821fd3ecfaf9
Jakub Bogusz <jbogusz03@gmail.com>
2024-02-23 14:23:56 +0100

    Creating auctions page, adding functionalities like paging, filtering

--- frontend/web-app/app/page.tsx
+++ frontend/web-app/app/page.tsx
@@ -1,8 +1,9 @@
+import Listings from "./auctions/Listings";
+
 export default function Home() {
   return (
     <div>
-      Listings
-      {/* <Listings /> */}
+      <Listings />
     </div>
   );
 }
