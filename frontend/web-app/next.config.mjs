2830edda74e59e5d9124da50e1d00b50af21bde8
Jakub Bogusz <jbogusz03@gmail.com>
2024-02-23 20:45:09 +0100

    Implement client authorization with next auth

--- frontend/web-app/next.config.mjs
+++ frontend/web-app/next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: ["cdn.pixabay.com"],
+  },
+  output: "standalone",
+};
 
 export default nextConfig;
