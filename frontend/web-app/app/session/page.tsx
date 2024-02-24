18ff9d86b4717c28fe4f9dab3bb552153554de8a
Jakub Bogusz <jbogusz03@gmail.com>
2024-02-24 11:42:08 +0100

    Get access token from account object in next jwt callback

--- frontend/web-app/app/session/page.tsx
+++ frontend/web-app/app/session/page.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Heading from "../components/Heading";
-import { getSession } from "../actions/authActions";
+import { getSession, getUserToken } from "../actions/authActions";
 import AuthTest from "./AuthTest";
 
 export default async function Session() {
   const session = await getSession();
+  const token = await getUserToken();
+
   return (
     <div>
       <Heading title="Session dashboard" />
@@ -14,6 +16,10 @@ export default async function Session() {
       </div>
       <div className="mt-4">
         <AuthTest />
+      </div>
+      <div className="bg-green-200 border-2 border-blue-500 mt-4">
+        <h3 className="text-lg">Token data</h3>
+        <pre className="overflow-auto">{JSON.stringify(token, null, 2)}</pre>
       </div>
     </div>
   );
