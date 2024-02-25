80bcfb33ebcab33e5e26af864160ec302e5f9ce6
Jakub Bogusz <jbogusz03@gmail.com>
2024-02-25 23:02:25 +0100

    Create AuctionsForm component with react hook form

--- frontend/web-app/app/auctions/create/page.tsx
+++ frontend/web-app/app/auctions/create/page.tsx
@@ -1,5 +1,16 @@
+import Heading from "@/app/components/Heading";
 import React from "react";
+import AuctionForm from "../AuctionForm";
 
 export default function Create() {
-  return <div>page</div>;
+  return (
+    <div className="mx-auto max-w-[75%] shadow-lg p-10 bg-white rounded-lg">
+      ASf
+      <Heading
+        title="Sell your car!"
+        subtitle="Please enter the details of your car"
+      />
+      <AuctionForm />
+    </div>
+  );
 }
