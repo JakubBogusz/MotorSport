568ed61c7c362b49af2b38fa671a2b6a9141b1d9
Jakub Bogusz <jbogusz03@gmail.com>
2024-02-25 21:41:50 +0100

    Add create and details routes to the app

--- frontend/web-app/app/nav/UserActions.tsx
+++ frontend/web-app/app/nav/UserActions.tsx
@@ -11,7 +11,7 @@ import { HiCog, HiUser } from "react-icons/hi2";
 import { useParamsStore } from "../hooks/useParamsStore";
 
 type Props = {
-  user: Partial<User>;
+  user: User;
 };
 
 export default function UserActions({ user }: Props) {
@@ -19,10 +19,24 @@ export default function UserActions({ user }: Props) {
   const pathname = usePathname();
   const setParams = useParamsStore((state) => state.setParams);
 
+  function setWinner() {
+    setParams({ winner: user.username, seller: undefined });
+    if (pathname !== "/") router.push("/");
+  }
+
+  function setSeller() {
+    setParams({ seller: user.username, winner: undefined });
+    if (pathname !== "/") router.push("/");
+  }
+
   return (
     <Dropdown inline label={`Welcome ${user.name}`}>
-      <Dropdown.Item icon={HiUser}>My Auctions</Dropdown.Item>
-      <Dropdown.Item icon={AiFillTrophy}>Auctions won</Dropdown.Item>
+      <Dropdown.Item icon={HiUser} onClick={setSeller}>
+        My Auctions
+      </Dropdown.Item>
+      <Dropdown.Item icon={AiFillTrophy} onClick={setWinner}>
+        Auctions won
+      </Dropdown.Item>
       <Dropdown.Item icon={AiFillCar}>
         <Link href="/auctions/create">Sell my car</Link>
       </Dropdown.Item>
